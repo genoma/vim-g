5c50e2e8d27ee0b2b30a787cbba097089e6d8e72
Alessandro Vioni <jenoma@gmail.com>
2014-03-17 17:00:35 +0100

    Update netrw-download whith instructions

--- netrw-download.sh
+++ netrw-download.sh
@@ -2,5 +2,8 @@
 cd ~/.vim/bundle/netrw/
 curl -O http://www.drchip.org/astronaut/vim/vbafiles/netrw.vba.gz
 gunzip netrw.vba.gz
-# vim +e ~/.vim/bundle/netrw/netrw.vba +UseVimball ~/.vim/bundle/netrw/
+
+# Install
+# :e ~/.vim/bundle/netrw/netrw.vba
+# UseVimball ~/.vim/bundle/netrw/
 
