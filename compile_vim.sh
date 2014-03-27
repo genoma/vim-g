2f2003d51df672496df405cba0ce8e785d7e5adf
Alessandro Vioni <jenoma@gmail.com>
2014-03-27 16:40:01 +0100

    Removet vim-startify

--- compile_vim.sh
+++ compile_vim.sh
@@ -4,10 +4,11 @@ cd ~/vim
 
 # make clean
 make distclean
+
 # complete compilation with lua/ruby/python
 # ./configure --enable-multibyte --with-tlib=ncurses --with-features=huge --enable-rubyinterp --enable-luainterp --enable-pythoninterp --with-lua-prefix=/usr/local --enable-gui=no --without-x --disable-netbeans --with-compiledby=jenoma@gmail.com --enable-fail-if-missing
 
-# minimal compilation
+# minimal compilation with huge no lua/ruby/python
 ./configure --enable-multibyte --with-tlib=ncurses --with-features=huge --enable-gui=no --without-x --disable-netbeans --with-compiledby=jenoma@gmail.com --enable-fail-if-missing
 
 make
