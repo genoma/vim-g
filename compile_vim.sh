8931ffff146aa587234b5bae7838c2eabf1c3340
Alessandro Vioni <jenoma@gmail.com>
2014-03-29 13:39:21 +0100

    Migrate compilation to MacPorts

--- compile_vim.sh
+++ compile_vim.sh
@@ -6,10 +6,9 @@ cd ~/vim
 make distclean
 
 # complete compilation with lua/ruby/python
-# ./configure --enable-multibyte --with-tlib=ncurses --with-features=huge --enable-rubyinterp --enable-luainterp --enable-pythoninterp --with-lua-prefix=/usr/local --enable-gui=no --without-x --disable-netbeans --with-compiledby=jenoma@gmail.com --enable-fail-if-missing
+./configure --enable-multibyte --with-tlib=ncurses --with-features=huge --enable-rubyinterp --enable-luainterp --enable-pythoninterp --with-lua-prefix=/opt/local --enable-gui=no --without-x --disable-netbeans --with-compiledby=jenoma@gmail.com --enable-fail-if-missing
 
 # minimal compilation with huge no lua/ruby/python
-./configure --enable-multibyte --with-tlib=ncurses --with-features=huge --enable-gui=no --without-x --disable-netbeans --with-compiledby=jenoma@gmail.com --enable-fail-if-missing
+# ./configure --enable-multibyte --with-tlib=ncurses --with-features=huge --enable-gui=no --without-x --disable-netbeans --with-compiledby=jenoma@gmail.com --enable-fail-if-missing
 
-make
-make install
+# make && make install
