19c13ee4812a15faa518050ab2a0b7e3fd49f020
Alessandro Vioni <jenoma@gmail.com>
2014-04-01 22:31:14 +0200

    compile_vim update

--- compile_vim.sh
+++ compile_vim.sh
@@ -5,10 +5,23 @@ cd ~/vim
 # make clean
 make distclean
 
-# complete compilation with lua/ruby/python
-./configure --enable-multibyte --with-tlib=ncurses --with-features=huge --enable-rubyinterp --enable-luainterp --enable-pythoninterp --with-lua-prefix=/opt/local --enable-gui=no --without-x --disable-netbeans --with-compiledby=jenoma@gmail.com --enable-fail-if-missing
+# complete compilation with lua/ruby/python/perl
+./configure --enable-multibyte\
+            --with-tlib=ncurses\
+            --with-features=huge\
+            --enable-rubyinterp\
+            --enable-luainterp\
+            --with-lua-prefix=/opt/local\
+            --enable-perlinterp\
+            --enable-pythoninterp\
+            --enable-gui=no\
+            --without-x\
+            --disable-netbeans\
+            --disable-nls\
+            --with-compiledby=jenoma@gmail.com\
+            --enable-fail-if-missing
 
 # minimal compilation with huge no lua/ruby/python
 # ./configure --enable-multibyte --with-tlib=ncurses --with-features=huge --enable-gui=no --without-x --disable-netbeans --with-compiledby=jenoma@gmail.com --enable-fail-if-missing
 
-make && make install
+# make && make install
